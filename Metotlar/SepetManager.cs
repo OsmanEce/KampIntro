c4ac75a32693cd14fd8fd9f0e076bc66b2081020
Osman Eceoğlu <osmaneceoglu@akdeniz.edu.tr>
2021-01-20 18:21:46 +0300

    16.01.2021

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // naming convention
+        //syntax
+        public void Ekle(Urun urun) // pythonda ki def
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi! : " + urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi) // bu iyi bir yazma şekli değil.
+        {
+        Console.WriteLine("Tebrikler.Sepete eklendi! : " +urunAdi);
+        }
+
+
+     }
+
+
+}
+
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+
+        // Propery - özellik
+        public int Id { get; set; } //prop tab tab string yaz tab bas Id yaz,  Id bir ürünü diğerinden ayırmaya yarar.
+
+        public string Adi { get; set; }
+
+        public double Fiyati { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public int StokAdedi { get; set; }
+    }
+}
